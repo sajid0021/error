9f093c6dfceb434f7f00fe438421c4e1d33d704a
sajid0021 <150411854+sajid0021@users.noreply.github.com>
2023-11-15 13:52:06 +0530

    Add files via upload

---
+++ post.component.ts
@@ -0,0 +1,22 @@
+import { 
+  Component, Input, EventEmitter, Output,OnInit
+ } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl:'./post.component.html',
+  styleUrls: ['./post.component.css']
+  
+})
+export class PostComponent implements OnInit{
+ @Input('img') postImg = ''
+ @Output() imgSelected = new EventEmitter<string>()
+constructor(){
+  console.log('constructor() called',this.postImg)
+}
+ngOnInit(){
+  console.log('ngOnInit() called',this.postImg)
+}
+}
+
+
